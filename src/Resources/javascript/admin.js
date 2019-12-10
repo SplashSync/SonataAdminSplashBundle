adc7ba39ed05dd518a6c63465998308561be8d09
BadPixxel <eshop.bpaquier@gmail.com>
2019-12-10 19:24:21 +0100

    Move to Webpack Encore

---
+++ src/Resources/javascript/admin.js
@@ -0,0 +1,23 @@
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) 2015-2019 Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+const $ = require('jquery');
+
+//------------------------------------------------------------------------------
+// Splash Widgets
+require('../../../../widgets-bundle/src/Resources/public/js/1-sparkline');
+require('../../../../widgets-bundle/src/Resources/public/js/2-morris');
+require('../../../../widgets-bundle/src/Resources/public/js/SplashWidgets');
+
+console.log('Splash Admin Webpack Encore done!');
