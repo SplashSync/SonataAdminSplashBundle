0fa81a2c82a8e9800d5c74b9e42c1628f3452826
BadPixxel <eshop.bpaquier@gmail.com>
2023-09-08 12:31:54 +0200

    WIP: SF5 & Sonata Admin 3 Migration

--- ci/verify.sh
+++ ci/verify.sh
@@ -22,17 +22,17 @@ echo '===> Start Docker Stack'
 docker-compose up -d
 
 ################################################################################
+# PHP 8.1
+echo '===> Checks Php 8.1'
+docker-compose exec php-8.1 bash ci/install.sh
+docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=travis
+docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=csfixer
+docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=phpstan
+
+################################################################################
 # PHP 8.0
 echo '===> Checks Php 8.0'
 docker-compose exec php-8.0 bash ci/install.sh
 docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=travis
 docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=csfixer
 docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 7.4
-echo '===> Checks Php 7.4'
-docker-compose exec php-7.4 bash ci/install.sh
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=phpstan
